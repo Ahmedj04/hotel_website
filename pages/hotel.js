a1aa0926b09522e52c128d8e88fcac8ee34e97eb
Ahmed <janahmed902@gmail.com>
2023-05-24 10:51:08 +0530

    commit

--- pages/hotel.js
+++ pages/hotel.js
@@ -69,7 +69,7 @@ function Hotel() {
                     <div className="absolute inset-0 bg-[url('https://themewagon.github.io/sogo/images/slider-7.jpg')] bg-no-repeat bg-cover bg-center"></div>
                     <div className="absolute inset-0 bg-black opacity-50"></div>
                 </div>
-                <header className="relative px-10 py-16 font-bold">
+                <header className="relative px-7 md:px-10 py-16 font-bold">
                     <div className="flex justify-between">
                         <h1 className="text-xl text-white md:text-2xl">{allHotelDetails?.property_name}</h1>
                         <div className='flex gap-8'>
@@ -286,7 +286,7 @@ function Hotel() {
             <section id='services' className="px-5 py-10">
                 <div className="  ">
                     <h2 className=" text-center font-semibold text-2xl md:text-4xl"> Property Services</h2>
-                    <div className="pl-10 py-10 grid mb-8 grid-flow-row-dense grid-cols-5 lg:grid-cols-5 md:grid-cols-4 md:col-span-9  grid-cols-2  md:gap-3 gap-1 lg:gap-3">
+                    <div className="py-10 grid grid-flow-row-dense grid-cols-5 lg:grid-cols-5 md:grid-cols-4 md:col-span-9  grid-cols-2  md:gap-3 gap-1 lg:gap-3">
                         {allHotelDetails?.services?.map((item, idx) => {
                             return (
                                 <React.Fragment key={idx}>
@@ -295,84 +295,84 @@ function Hotel() {
                                             case 'ser001': return (
                                                 <div>
                                                     {/*AC*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.ac}
                                                     </span>
                                                 </div>)
                                             case 'ser002': return (
                                                 <div>
                                                     {/*All Inclusive Available*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.inclusive}
                                                     </span>
                                                 </div>)
                                             case 'ser003': return (
                                                 <div>
                                                     {/*Child Friendly*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.childfriendly}
                                                     </span>
                                                 </div>)
                                             case 'ser004': return (
                                                 <div>
                                                     {/*Golf Course*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.golf}
                                                     </span>
                                                 </div>)
                                             case 'ser005': return (
                                                 <div>
                                                     {/*Airport Shuttle*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.airport}
                                                     </span>
                                                 </div>)
                                             case 'ser006': return (
                                                 <div>
                                                     {/*Bar Lounge*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.bar}
                                                     </span>
                                                 </div>)
                                             case 'ser007': return (
                                                 <div>
                                                     {/*Beach*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.beach}
                                                     </span>
                                                 </div>)
                                             case 'ser008': return (
                                                 <div>
                                                     {/*Business Center*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.bussinesscenter}
                                                     </span>
                                                 </div>)
                                             case 'ser009': return (
                                                 <div>
                                                     {/*Fitness Center*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.fitnesscenter}
                                                     </span>
                                                 </div>)
                                             case 'ser0010': return (
                                                 <div>
                                                     {/*Free Breakfast*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.breakfast}
                                                     </span>
                                                 </div>)
                                             case 'ser0011': return (
                                                 <div>
                                                     {/*Hot Tub*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.hottub}
                                                     </span>
                                                 </div>)
                                             case 'ser0012': return (
                                                 <div>
                                                     {/*Laundary Service*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.laundary}
                                                     </span>
                                                 </div>)
@@ -380,35 +380,35 @@ function Hotel() {
                                             case 'ser0013': return (
                                                 <div>
                                                     {/*Restaurant*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.restaurant}
                                                     </span>
                                                 </div>)
                                             case 'ser0014': return (
                                                 <div>
                                                     {/*Room Service*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.roomservice}
                                                     </span>
                                                 </div>)
                                             case 'ser0015': return (
                                                 <div>
                                                     {/*Spa*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.spa}
                                                     </span>
                                                 </div>)
                                             case 'ser0016': return (
                                                 <div>
                                                     {/*Kitchen*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.kitchen}
                                                     </span>
                                                 </div>)
                                             case 'ser0017': return (
                                                 <div>
                                                     {/*Parking*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.parking}
                                                     </span>
                                                 </div>)
@@ -416,35 +416,35 @@ function Hotel() {
                                             case 'ser0018': return (
                                                 <div>
                                                     {/*Pets Allowed*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.pets}
                                                     </span>
                                                 </div>)
                                             case 'ser0019': return (
                                                 <div>
                                                     {/*Smoke Free*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.smokefree}
                                                     </span>
                                                 </div>)
                                             case 'ser0020': return (
                                                 <div>
                                                     {/*Swimming Pool*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.pool}
                                                     </span>
                                                 </div>)
                                             case 'ser0021': return (
                                                 <div>
                                                     {/*Wheel Chair*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.wheelchair}
                                                     </span>
                                                 </div>)
                                             case 'ser0022': return (
                                                 <div>
                                                     {/*Wifi Type*/}
-                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 " title={item?.local_service_name}>
+                                                    <span className="tooltip rounded-full hover:cursor-pointer hover:text-gray-900 text-gray-600 flex justify-center" title={item?.local_service_name}>
                                                         {icon?.Icons?.[i]?.wifi}
                                                     </span>
                                                 </div>)
