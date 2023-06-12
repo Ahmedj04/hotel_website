bb4bb42e228e77a462658726d045506d69529f8d
Ahmed <janahmed902@gmail.com>
2023-06-12 13:43:41 +0530

    adjusted position

--- components/NewTheme/Home/index.js
+++ components/NewTheme/Home/index.js
@@ -42,7 +42,7 @@ function Home({ allHotelDetails, menu, setMenu, hotelDetailLoader, lang, setLang
                         top: 4rem;
                     }
                 }
-                @media (min-height: 760px) and (max-height:820px) {
+                @media (min-height: 760px) and (max-height:850px) {
                     .contentBox {
                         position:relative;
                         top: 6rem;
